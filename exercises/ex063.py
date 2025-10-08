a48543a91e4827f2b8582f32591536be0ae8b9ce
lucas-fertonani <lucassenciales@gmail.com>
2025-10-08 20:10:29 -0300

    feat: add exercise 63

--- exercises/ex063.py
+++ exercises/ex063.py
@@ -2,13 +2,24 @@
 #Escreva um programa que leia um numero n inteiro qualquer e mostre na tela os n primeiros elementos de uma sequencia de
 #Fibonacci.
 #ex: 0 -> 1 -> 1 -> 2 -> 3 -> 5 -> 8
-quantity = int(input('Digite um numero: '))
-count = 2
-array = [0,1]
 sequencia = []
-if count < quantity:
+quantity = int(input("Digite a quantidade de fibonacci desejada: "))
+count = 0
+
+while True:
+    if quantity<=count:
+        break
+
     count += 1
-if quantity > sequencia:
-    sequencia = count + array.append(-1)
-    return sequencia
+
+    if count == 0:
+        sequencia.append(0)
+
+    elif count == 1:
+        sequencia.append(1)
+
+    else:
+        sequencia.append(sequencia[len(sequencia)-2]+sequencia[len(sequencia)-1])
+    
+
 print(sequencia)
