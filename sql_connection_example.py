91dfc2401852ac3a596956d620a2d2c0f307cd10
lucas-fertonani <lucassenciales@gmail.com>
2025-11-05 21:44:51 -0300

    chore: SQL connection with python example

---
+++ sql_connection_example.py
@@ -0,0 +1,46 @@
+# SQL conneciton usage (example)
+
+import psycopg2
+
+def get_conn():
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5432,
+        database="postgres",
+        user="postgres",
+        password="postgres"
+    )
+    return conn
+
+def save_record_on_memory(name: str, age: int, gender: str):
+    conn = get_conn()
+    cursor = conn.cursor()
+    cursor.execute(
+        "INSERT INTO users (name, age, gender) VALUES (%s, %s, %s)",
+        (name, age, gender)
+    )
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+def get_records():
+    conn = get_conn()
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT * FROM users"
+    )
+    records = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return records
+
+
+# count_woman = 0
+# while True:
+#     name = str(input('Nome: '))
+#     age = int(input('Idade: '))
+#     gender = str(input('Sexo: [M/F]: ')).upper()
+#     save_record_on_memory(name, age, gender)
+
+
+# save_record_on_memory('Carlos', 71, 'M')
