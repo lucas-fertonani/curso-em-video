6729a2244b15c650d22bf10a9dd61e1dce629581
lucas-fertonani <lucassenciales@gmail.com>
2025-11-08 16:40:29 -0300

    feat: Funções Aprender o que são funções

---
+++ class/Aula 020 explicação.py
@@ -0,0 +1,50 @@
+#Aula 020
+#Funções
+#Funções é rotina e uma alguma coisa que você faz constantemente por exemplo tabela:
+#mostrartabela()
+#       |       |
+#       |       |
+#       |       |
+#       |       |
+
+#No python a função se chama "def",def = definição de função
+#Ou seja função e criar e uma função personalizada que não existe no python.
+#Exemplo:
+print('----------------------------------')
+print('        Cadastro de Saúde         ')
+print('----------------------------------')
+print('----------------------------------')
+print('        Lista de Espera           ')
+print('----------------------------------')
+#inves de repetir a linha com traço 4 vezes poderemos usar as funções ou seja:
+def rotinalinha():
+    print('-------------------------------')
+#agora com o rotinalinha() fica assim:
+rotinalinha()
+print('         Cadastro de Saúde         ')
+rotinalinha()
+rotinalinha()
+print('         Lista de Espera           ')
+rotinalinha()
+#ele fica a mesma coisa do outro exemplo so que menos repetitivo
+
+
+#A gente pode usar a estrategia do parametro do def como voce pode ver
+#o bloco de cima que cadastro de saude é muito parecido com lista de espera mas so muda o que esta dentro desse bloco ou seja no meio entre as duas linhas.
+#Então inves de:
+print('----------------------------------')
+print('        Cadastro de Saúde         ')
+print('----------------------------------')
+print('----------------------------------')
+print('        Lista de Espera           ')
+print('----------------------------------')
+#Com o parametro ficaria assim:
+def message(mensg):
+    print('-----------------------------')
+    print(mensg)
+    print('-----------------------------')
+
+
+message('ALISTAMENTO MILITAR')
+message('TOP 10 COMIDAS DO BRASIL')
+message('AS 7 MARAVILHAS DO MUNDO')
