14859663089809123c90e08e6412032403f5324d
lucas-fertonani <lucassenciales@gmail.com>
2025-11-14 19:51:24 -0300

    chore: adding pedences

--- class/Aula 023 explicação.py
+++ class/Aula 023 explicação.py
@@ -9,3 +9,32 @@ except:
     falhou
 
 #Se eu tentar a operação de cima e falhar o que vai acontecer é assim que funciona o try e except
+#e se dar certo usaremos outro comando que é:
+try:
+    operation
+except:
+    error
+else:
+    print(operation)
+
+#O finally eu uso quando não importa se deu certo ou falhou veja:
+try:
+    operation
+except:
+    error
+else:
+    print(operation)
+finally:
+    print('<<<Foi bom sua companinha na calculadora!>>>')
+
+#Para mostrar o erro eu uso o 'Exception as erro':
+try:
+    operation
+except Exception as erro:
+    error {erro.__cause__}
+else:
+    print(operation)
+finally:
+    print('<<<Foi bom sua companinha na calculadora!>>>')
+
+#Eu posso ter varios tipos de except pra um try e cada um deles vai ter sua bloco, erro e mensagem
