1e90deedb8209409f882425bab1d42964cf0ee91
lucas-fertonani <lucassenciales@gmail.com>
2025-11-14 19:52:03 -0300

    chore: adding pedences

--- practice/Aula 023 prática.py
+++ practice/Aula 023 prática.py
@@ -5,6 +5,23 @@ try:
     n_1 = int(input('Numero 1: '))
     n_2 = int(input('Numero 2: '))
     conta = n_1 - n_2
-except:
-    print('Error! This is not a int! Try Again')
-print(conta)
+except Exception as error:
+    print(f'Error! This is not a int! the cause of the error is {error.__class__}')
+else:
+    print(conta)
+finally:
+    print('<<<Foi bom sua companinha na calculadora!>>>')
+
+
+try:
+    n_1 = int(input('Numero 1: '))
+    n_2 = int(input('Numero 2: '))
+    conta = n_1 - n_2
+except (ValueError):
+    print(f'Erro! esse numero que voce digitou nao tem é um int(número inteiro)')
+except (SyntaxError):
+    print(f'Isso não é um número!')
+else:
+    print(conta)
+finally:
+    print('<<<Foi bom sua companinha na calculadora!>>>')
