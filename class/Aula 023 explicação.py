c531e15d76c92586be7f94bf79ba3ed740a7afd3
lucas-fertonani <lucassenciales@gmail.com>
2025-11-14 19:48:02 -0300

    feat: Aprendendo o que são tratamentos de erros e exceções

---
+++ class/Aula 023 explicação.py
@@ -0,0 +1,11 @@
+#Aula 023
+#Tratamento de Erros e Exceções
+
+#Exception -> exceção
+#Para tratar execeções no python usaremos o comando: try: , except:         try->tente
+try:
+    operação
+except:
+    falhou
+
+#Se eu tentar a operação de cima e falhar o que vai acontecer é assim que funciona o try e except
